e53beabe1479faa6139665a3caca84d4850a38e3
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2022-05-06 14:41:45 +0300

    Add .mlk folder

--- .mlk/rollup.config.js
+++ .mlk/rollup.config.js
@@ -1,11 +1,12 @@
 import fs from 'fs'
+import path from 'path'
 import {swc} from "rollup-plugin-swc3"
 import {nodeResolve} from "@rollup/plugin-node-resolve"
 import commonjs from '@rollup/plugin-commonjs'
 import svgr from '@svgr/rollup'
 import postcss from 'rollup-plugin-postcss'
 
-const swcrc = JSON.parse(fs.readFileSync('./.swcrc'))
+const swcrc = JSON.parse(fs.readFileSync(path.resolve(__dirname, '.swcrc')))
 
 export default {
   input: './src/index.tsx',
@@ -15,7 +16,7 @@ export default {
       extensions: [".css"],
       modules: true,
       config: {
-        path: './postcss.config.js'
+        path: path.resolve(__dirname,'../postcss.config.js')
       }
     }),
     nodeResolve({ extensions: [".js", ".jsx", ".ts", ".tsx"] }),
